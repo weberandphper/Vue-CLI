84a18fb8a181f71dda82ca08989320b81b89f76e
一朵浪花 <cfw1034846940@gmail.com>
2019-09-02 19:00:59 +0800

    1

--- src/router.js
+++ src/router.js
@@ -25,3 +25,37 @@ export default new Router({
     { path: '*', redirect: '/404' }
   ]
 })
+
+/* 准备动态添加的路由 */
+export const DynamicRoutes = [
+  {
+    path: '',
+    component: Layout,
+    name: 'container',
+    redirect: 'home',
+    meta: {
+      requiresAuth: true,
+      name: '首页'
+    },
+    children: [
+      {
+        id: 1,
+        path: 'home',
+        component: Home,
+        name: 'home',
+        meta: {
+          name: '首页',
+          icon: 'tree'
+        }
+      }
+    ]
+  },
+  {
+    path: '/403',
+    component: Forbidden
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
+]
